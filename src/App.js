72a0709d3e9cca20307c0788e1980ad32e41cbff
Tyler Johnson <tyler@csdconsultants.us>
2021-12-20 14:55:32 -0700

    creates images directory and adds sample image, imports to App

--- src/App.js
+++ src/App.js
@@ -2,13 +2,30 @@ import React from 'react';
 import './App.css';
 import { Container, AppBar, Typography, Grow, Grid } from '@material-ui/core';
 
+import Posts from './components/Posts/posts';
+import Form from './components/Form/form';
+
+import postimg from './images/sampleimage.jpg'
+
 const App = () => {
   return (
     <Container maxwidth='lg'>
       <AppBar position="static" color="inherit">
         <Typography variant="h2" align='center'>Note Posts</Typography>
-        <img src={posts} alt="posts" height="60" />
+        <img src={postimg} alt="posts" height="60" />
       </AppBar>
+      <Grow in>
+        <Container>
+          <Grid container justify='space-between' alignItems="stretch" spacing={3}>
+            <Grid item xs={12} sm={7}>
+              <Posts />
+            </Grid>
+            <Grid item xs={12} sm={4}>
+              <Form />
+            </Grid>
+          </Grid>
+        </Container>
+      </Grow>
     </Container>
   )
 }
