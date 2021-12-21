4915992fe35810bf03f900b5e9d1bc736b420961
Tyler Johnson <tyler@csdconsultants.us>
2021-12-20 22:14:11 -0700

    imports useDispatch and createPost, adds each to handleSubmit function

--- src/components/Form/form.js
+++ src/components/Form/form.js
@@ -1,8 +1,10 @@
 import React, { useState } from 'react';
 import { TextField, Button, Typography, Paper } from '@material-ui/core';
 import FileBase from 'react-file-base64';
+import { useDispatch } from 'react-redux';
 
 import useStyles from './styles.js';
+import { createPost } from '../../actions/posts.js';
 
 const Form = () => {
   const [postData, setPostData] = useState({
@@ -13,13 +15,15 @@ const Form = () => {
   })
 
   const classes = useStyles();
+  const dispatch = useDispatch();
 
-  const handleSubmit = () => {
-
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    dispatch(createPost(postData))
   }
 
   const clear = () => {
-    
+
   }
 
   return (
