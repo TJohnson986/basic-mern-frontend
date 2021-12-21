e6e4247ba52c0950162a3a178ef0ff5f6dc23bbc
Tyler Johnson <tyler@csdconsultants.us>
2021-12-20 20:49:12 -0700

    imports useSelector to retreive state.posts

--- src/components/Posts/posts.js
+++ src/components/Posts/posts.js
@@ -1,10 +1,14 @@
 import React from 'react';
-import Post from './Post/post';
+import { useSelector } from 'react-redux';
 
+import Post from './Post/post';
 import useStyles from './styles.js';
 
 const Posts = () => {
+  const posts = useSelector((state) => state.posts )
   const classes = useStyles();
+
+  console.log('fetched posts', posts);
 
     return (
         <>
