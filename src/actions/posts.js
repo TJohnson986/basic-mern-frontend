bb6830567057d5cd2c934364db720844dae3c97d
Tyler Johnson <tyler@csdconsultants.us>
2021-12-20 20:29:34 -0700

    corrects spelling mistake

--- src/actions/posts.js
+++ src/actions/posts.js
@@ -3,7 +3,7 @@ import * as api from '../api';
 export const getPosts = () => async (dispatch) => {
     try{
         const { data } = await api.fetchPosts();
-        dispatch({ type: 'FETCH_ALL', paylaod: data })
+        dispatch({ type: 'FETCH_ALL', payload: data })
     } catch (error) {
         console.log(error.message);
     }
