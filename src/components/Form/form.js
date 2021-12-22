686ca7254fb7f39f746648e2e12c95383159ccbc
Tyler Johnson <tyler@csdconsultants.us>
2021-12-22 15:49:14 -0700

    passes in currentId prop, creates conditional in handleSubmit to either dispatch update or create

--- src/components/Form/form.js
+++ src/components/Form/form.js
@@ -4,9 +4,9 @@ import FileBase from 'react-file-base64';
 import { useDispatch } from 'react-redux';
 
 import useStyles from './styles.js';
-import { createPost } from '../../actions/posts.js';
+import { createPost, updatePost } from '../../actions/posts.js';
 
-const Form = () => {
+const Form = ({ currentId, setCurrentId }) => {
   const [postData, setPostData] = useState({
     creator: '',
     title: '',
@@ -19,7 +19,12 @@ const Form = () => {
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    dispatch(createPost(postData))
+
+    if(currentId) {
+      dispatch(updatePost(currentId, postData))  
+    } else {
+      dispatch(createPost(postData));
+    }
   }
 
   const clear = () => {
