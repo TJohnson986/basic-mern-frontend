20e97650d31f2d99877e7a1f791f4c47933861bd
Tyler Johnson <tyler@csdconsultants.us>
2021-12-23 10:28:03 -0700

    adds DELETE reducer

--- src/reducers/posts.js
+++ src/reducers/posts.js
@@ -7,6 +7,8 @@ export default (posts = [], action) => {
         default:
             return posts;
         case 'UPDATE':
-            return posts.map((post) => post._id === action.payload._id ? action.payload : post)
+            return posts.map((post) => post._id === action.payload._id ? action.payload : post);
+        case 'DELETE':
+            return posts.filter((post) => post._id !== action.payload);
     }
 }
