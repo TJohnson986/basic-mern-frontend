018121a4c336474965255c8e5df08f044875c7e1
Tyler Johnson <tyler@csdconsultants.us>
2021-12-23 20:37:28 -0700

    adds LIKE case, using UPDATE return logic

--- src/reducers/posts.js
+++ src/reducers/posts.js
@@ -7,6 +7,7 @@ export default (posts = [], action) => {
         default:
             return posts;
         case 'UPDATE':
+        case 'LIKE':
             return posts.map((post) => post._id === action.payload._id ? action.payload : post);
         case 'DELETE':
             return posts.filter((post) => post._id !== action.payload);
